622ccfae40de0a95a7af893a49d7bbe9c91a644f
CiaoName <65759143+BlameTwo@users.noreply.github.com>
2024-04-03 18:00:59 +0800

    commit 转换器有错误

---
+++ Aria2.Client/Converters/BackBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Aria2.Client.Converters;
+
+/// <summary>
+/// 反转布尔
+/// </summary>
+public class BackBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool v)
+            return !v;
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        if (value is bool v)
+            return !v;
+        return value;
+    }
+}
